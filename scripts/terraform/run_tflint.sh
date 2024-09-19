b8605429298de4f1313db6c3a1275b26983d3789
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-09-19 16:32:04 +0300

    mv tflint init under run_tflint

--- scripts/terraform/run_tflint.sh
+++ scripts/terraform/run_tflint.sh
@@ -8,5 +8,6 @@ else
   TFLINT_CONFIG=$GITHUB_ACTION_PATH/configs/.tflint.hcl
 fi
 
-tflint --config "$TFLINT_CONFIG" --chdir "$GITHUB_WORKSPACE" --recursive | tee "$GITHUB_WORKSPACE"/reports/terraform/tflint.log
+tflint --config "$TFLINT_CONFIG" --init | tee "$GITHUB_WORKSPACE"/reports/terraform/tflint.log
+tflint --config "$TFLINT_CONFIG" --chdir "$GITHUB_WORKSPACE" --recursive | tee -a "$GITHUB_WORKSPACE"/reports/terraform/tflint.log
 uci_logger terraform tflint $?
